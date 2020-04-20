ab10139c57861bea310cf832704d6e019ad10f13
mayumi-ohno <mayumi.ono@rakus-partners.co.jp>
2020-04-20 10:32:23 +0900

    お気に入りボタンをマウスオーバーで切り替えるよう更新

---
+++ src/main/resources/static/js/question_list.js
@@ -0,0 +1,39 @@
+$(function() {
+	
+	 $("a[name$=mark]").hover(function() {
+			var id = $(this).attr("name").replace("mark", "");
+			var markStatus = $("input[id="+id+"mark-status]");
+  				if(markStatus.val() == '★'){
+  					$(this).text("☆");
+  				}else{
+  					$(this).text("★");
+  				}
+		  }, function() {
+			var id = $(this).attr("name").replace("mark", "");
+			var markStatus = $("input[id="+id+"mark-status]");
+  			$(this).text(markStatus.val());
+		});
+	  
+	  $("a[name$=mark]").on("click", function(){
+// 			var hostUrl = "http://tsuchiya-box.work/questions/tsuchiya/mark";
+			var hostUrl = "http://localhost:8080/tsuchiya/mark";
+			var id = $(this).attr("name").replace("mark", "");
+			var markStatus = $("input[id="+id+"mark-status]");
+			
+				$.ajax({
+					url: hostUrl,
+					type: "GET",
+					dataType: "json",
+					data:{
+						id: id ,
+						mark: markStatus.val(),
+					},
+					async: true
+				});
+  				if(markStatus.val() == '★'){
+  					markStatus.val("☆");
+  				}else{
+  					markStatus.val("★");
+  				}
+	  });
+});
